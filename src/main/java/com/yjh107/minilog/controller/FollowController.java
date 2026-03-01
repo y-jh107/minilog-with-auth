9ef7ac9cc07d7e454efc7a295fea689f57cc1bda
양진혁 <jin99545@gmail.com>
2026-03-01 14:29:40 +0900

    refactor: FollowController 수정

--- src/main/java/com/yjh107/minilog/controller/FollowController.java
+++ src/main/java/com/yjh107/minilog/controller/FollowController.java
@@ -2,12 +2,14 @@ package com.yjh107.minilog.controller;
 
 import com.yjh107.minilog.dto.FollowRequestDto;
 import com.yjh107.minilog.dto.FollowResponseDto;
+import com.yjh107.minilog.security.MinilogUserDetails;
 import com.yjh107.minilog.service.FollowService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,12 +20,12 @@ import java.util.List;
     /api/v1/follow 엔드포인트로 접근
  */
 @RestController
-@RequestMapping("/api/v1/follow")
-public class FollowContoller {
+@RequestMapping("/api/v2/follow")
+public class FollowController {
     private final FollowService followService;
 
     @Autowired
-    public FollowContoller(FollowService followService) {
+    public FollowController(FollowService followService) {
         this.followService = followService;
     }
 
@@ -33,22 +35,26 @@ public class FollowContoller {
             @ApiResponse(responseCode = "200", description = "성공"),
             @ApiResponse(responseCode = "404", description = "사용자 없음"),
     })
-    public ResponseEntity<FollowResponseDto> follow(@RequestBody FollowRequestDto request) {
-        Long followerId = request.getFollowerId();
+    public ResponseEntity<FollowResponseDto> follow(
+            @AuthenticationPrincipal MinilogUserDetails userDetails,
+            @RequestBody FollowRequestDto request) {
+        Long followerId = userDetails.getId();
         Long followeeId = request.getFolloweeId();
 
         FollowResponseDto follow = followService.follow(followerId, followeeId);
         return ResponseEntity.ok(follow);
     }
 
-    @DeleteMapping("/{followerId}/{followeeId}")
+    @DeleteMapping("/{followeeId}")
     @Operation(summary = "언팔로우")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "성공"),
             @ApiResponse(responseCode = "404", description = "사용자 없음"),
     })
-    public ResponseEntity<Void> unfollow(@PathVariable Long followerId, @PathVariable Long followeeId) {
-        followService.unfollow(followerId, followeeId);
+    public ResponseEntity<Void> unfollow(
+            @AuthenticationPrincipal MinilogUserDetails userDetails,
+            @PathVariable Long followeeId) {
+        followService.unfollow(userDetails.getId(), followeeId);
         return ResponseEntity.ok().build();
     }
 
