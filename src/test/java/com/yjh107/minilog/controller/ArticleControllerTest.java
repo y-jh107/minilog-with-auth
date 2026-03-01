431a755f2df373f0946c96d0ebc970d12cd51476
양진혁 <jin99545@gmail.com>
2026-03-01 14:34:54 +0900

    test: Controller 관련 테스트케이스 작성

--- src/test/java/com/yjh107/minilog/controller/ArticleControllerTest.java
+++ src/test/java/com/yjh107/minilog/controller/ArticleControllerTest.java
@@ -1,4 +1,186 @@
 package com.yjh107.minilog.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yjh107.minilog.dto.ArticleRequestDto;
+import com.yjh107.minilog.dto.ArticleResponseDto;
+import com.yjh107.minilog.exception.ArticleNotFoundException;
+import com.yjh107.minilog.security.JwtUtil;
+import com.yjh107.minilog.security.MinilogUserDetails;
+import com.yjh107.minilog.service.ArticleService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(ArticleController.class) // 웹 레이어만 테스트
+@MockBean(JpaMetamodelMappingContext.class)
 public class ArticleControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ArticleService articleService;
+
+    @MockBean
+    private JwtUtil jwtUtil; // JwtUtil 의존성 주입
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private LocalDateTime fixtureDateTime = LocalDateTime.of(2025, 1, 1, 0, 0, 0);
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+    private String formattedFixtureDateTime = fixtureDateTime.format(formatter);
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+
+        MinilogUserDetails userDetails =
+                new MinilogUserDetails(1L, "testuser", "password", List.of(() -> "ROLE_AUTHOR"));
+        UsernamePasswordAuthenticationToken auth =
+                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+    }
+
+    @Test
+    public void testCreateArticle() throws Exception {
+        ArticleRequestDto requestDto = ArticleRequestDto.builder().content("Test Content").build();
+        ArticleResponseDto responseDto =
+                ArticleResponseDto.builder()
+                        .articleId(1L)
+                        .content("Test Content")
+                        .authorId(1L)
+                        .authorName("testuser")
+                        .createdAt(fixtureDateTime)
+                        .build();
+        when(articleService.createArticle(any(String.class), anyLong())).thenReturn(responseDto);
+
+        mockMvc
+                .perform(
+                        post("/api/v2/article")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(requestDto))
+                                .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.articleId").value(1L))
+                .andExpect(jsonPath("$.content").value("Test Content"))
+                .andExpect(jsonPath("$.authorId").value(1L))
+                .andExpect(jsonPath("$.authorName").value("testuser"))
+                .andExpect(jsonPath("$.createdAt").value(formattedFixtureDateTime));
+    }
+
+    @Test
+    public void testGetArticle() throws Exception {
+        ArticleResponseDto responseDto =
+                ArticleResponseDto.builder()
+                        .articleId(1L)
+                        .content("Test Content")
+                        .authorId(1L)
+                        .authorName("testuser")
+                        .createdAt(fixtureDateTime)
+                        .build();
+        when(articleService.getArticleById(anyLong())).thenReturn(responseDto);
+
+        mockMvc
+                .perform(get("/api/v2/article/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.articleId").value(1L))
+                .andExpect(jsonPath("$.content").value("Test Content"))
+                .andExpect(jsonPath("$.authorId").value(1L))
+                .andExpect(jsonPath("$.authorName").value("testuser"))
+                .andExpect(jsonPath("$.createdAt").value(formattedFixtureDateTime));
+    }
+
+    @Test
+    public void testUpdateArticle() throws Exception {
+        Long userId = 1L;
+        Long articleId = 1L;
+        String updatedContent = "Updated Content";
+        ArticleRequestDto requestDto = ArticleRequestDto.builder().content("Test Content").build();
+        ArticleResponseDto responseDto =
+                ArticleResponseDto.builder()
+                        .articleId(articleId)
+                        .content(updatedContent)
+                        .authorId(userId)
+                        .authorName("testuser")
+                        .createdAt(fixtureDateTime)
+                        .build();
+        when(articleService.updateArticle(anyLong(), anyLong(), any(String.class)))
+                .thenReturn(responseDto);
+
+        mockMvc
+                .perform(
+                        put("/api/v2/article/1")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(requestDto))
+                                .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.articleId").value(articleId))
+                .andExpect(jsonPath("$.content").value(updatedContent))
+                .andExpect(jsonPath("$.authorId").value(userId))
+                .andExpect(jsonPath("$.authorName").value("testuser"))
+                .andExpect(jsonPath("$.createdAt").value(formattedFixtureDateTime));
+    }
+
+    @Test
+    public void testDeleteArticle() throws Exception {
+        mockMvc.perform(delete("/api/v2/article/1").with(csrf())).andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void testGetArticleByUserId() throws Exception {
+        Long userId = 1L;
+        Long articleId = 1L;
+        ArticleResponseDto responseDto =
+                ArticleResponseDto.builder()
+                        .articleId(articleId)
+                        .content("Test Content")
+                        .authorId(userId)
+                        .authorName("testuser")
+                        .createdAt(fixtureDateTime)
+                        .build();
+        List<ArticleResponseDto> responseList = Collections.singletonList(responseDto);
+        when(articleService.getArticleListByUserId(anyLong())).thenReturn(responseList);
+
+        mockMvc
+                .perform(get("/api/v2/article").param("authorId", "1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].articleId").value(articleId))
+                .andExpect(jsonPath("$[0].content").value("Test Content"))
+                .andExpect(jsonPath("$[0].authorId").value(userId))
+                .andExpect(jsonPath("$[0].authorName").value("testuser"))
+                .andExpect(jsonPath("$[0].createdAt").value(formattedFixtureDateTime));
+    }
+
+    @Test
+    public void testGlobalExceptionHandler() throws Exception {
+        when(articleService.getArticleById(anyLong()))
+                .thenThrow(new ArticleNotFoundException("Article Not Found"));
+
+        mockMvc
+                .perform(get("/api/v2/article/999"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("Article Not Found"));
+    }
 }
